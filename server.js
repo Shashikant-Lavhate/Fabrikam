49a6942a5d5c6e2971a505f205003bf763b0d6ea
unknown <akashdighe19@gmail.com>
2023-07-28 08:53:43 +0530

    Fabrikam project for module end exam

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+
+app.use(express.static(path.join(__dirname, 'Public')));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(___dirname + '/index.html'));
+});
+app.get('/login', function (req, res) {
+    res.sendFile(path.join(___dirname + '/login.html'));
+});
+app.get('/about', function (req, res) {
+    res.sendFile(path.join(___dirname + '/about.html'));
+});
+app.get('/contact', function (req, res) {
+    res.sendFile(path.join(___dirname + '/contact.html'));
+});
+
+var server = app.listen(3339, function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Example app listening at http://localhost:3339", host, port)
+});
